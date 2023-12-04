f495d521e3d8e49333c20bb50c80837e159d2eb3
Niko Carpenter <niko@nikocarpenter.com>
2023-12-03 22:14:29 -0600

    DRY the code a bit

--- zig/2023/day03/src/root.zig
+++ zig/2023/day03/src/root.zig
@@ -51,86 +51,30 @@ fn part2(input: []const u8) !u32 {
     var lines_iter = std.mem.tokenizeScalar(u8, input, '\n');
     var prev_line: ?[]const u8 = null;
     var sum: u32 = 0;
-    var line_num: usize = 0;
     while (lines_iter.next()) |line| : (prev_line = line) {
-        line_num += 1;
         const next_line = lines_iter.peek();
         for (line, 0..) |ch, i| {
             if (ch != '*') continue;
-            var numbers_count: usize = 0;
-            var ratio: u32 = 1;
+            var number_count = NumberCount{};
 
             // See if this "geer" is touching exactly two numbers (diagonals included).
-
-            // Look left.
-            if (i > 0) {
-                if (wholeNumber(line, i - 1)) |num| {
-                    numbers_count += 1;
-                    ratio *= num;
-                }
-            }
-            // Look right.
-            if (i < line.len - 1) {
-                if (wholeNumber(line, i + 1)) |num| {
-                    numbers_count += 1;
-                    ratio *= num;
-                }
-            }
-
-            // If there is a digit above or below,
-            // we don't want to check the diagonals on either side,
-            // since they'll either be a part of the same number, or not a digit,
-            // and we don't want to double count the same number.
-
+            const left_right = numberCountLeftRight(line, i, false);
+            number_count.count += left_right.count;
+            number_count.product *= left_right.product;
             // Look above.
             if (prev_line) |l| {
-                if (wholeNumber(l, i)) |num| {
-                    numbers_count += 1;
-                    ratio *= num;
-                } else {
-                    // Search diagonals on either side.
-                    // Look above and left.
-                    if (i > 0) {
-                        if (wholeNumber(l, i - 1)) |num| {
-                            numbers_count += 1;
-                            ratio *= num;
-                        }
-                    }
-                    // Look above and right.
-                    if (i < l.len - 1) {
-                        if (wholeNumber(l, i + 1)) |num| {
-                            numbers_count += 1;
-                            ratio *= num;
-                        }
-                    }
-                }
+                const above = numberCountLeftRight(l, i, true);
+                number_count.count += above.count;
+                number_count.product *= above.product;
             }
-
             // Look below.
             if (next_line) |l| {
-                if (wholeNumber(l, i)) |num| {
-                    numbers_count += 1;
-                    ratio *= num;
-                } else {
-                    // Search diagonals on either side.
-                    // Look below and left.
-                    if (i > 0) {
-                        if (wholeNumber(l, i - 1)) |num| {
-                            numbers_count += 1;
-                            ratio *= num;
-                        }
-                    }
-                    // Look below and right.
-                    if (i < l.len - 1) {
-                        if (wholeNumber(l, i + 1)) |num| {
-                            numbers_count += 1;
-                            ratio *= num;
-                        }
-                    }
-                }
+                const below = numberCountLeftRight(l, i, true);
+                number_count.count += below.count;
+                number_count.product *= below.product;
             }
 
-            if (numbers_count == 2) sum += ratio;
+            if (number_count.count == 2) sum += number_count.product;
         }
     }
 
@@ -163,6 +107,51 @@ fn wholeNumber(s: []const u8, pos: usize) ?u32 {
     while (start > 0 and s[start - 1] >= '0' and s[start - 1] <= '9') : (start -= 1) {}
     var parser = std.fmt.Parser{ .buf = s, .pos = start };
     return @intCast(parser.number() orelse unreachable);
+}
+
+const NumberCount = struct {
+    count: usize = 0,
+    product: u32 = 1,
+};
+
+// Searches around a character,
+// returning a partial NumberCount indicating the count and product of any found numbers.
+// If includePos is true,
+// the character at s[pos] will also be tested.
+// includePos should be false when searching the line containing the target character,
+// true otherwise.
+fn numberCountLeftRight(s: []const u8, pos: usize, includePos: bool) NumberCount {
+    var number_count = NumberCount{};
+
+    if (includePos) {
+        if (wholeNumber(s, pos)) |num| {
+            number_count.count = 1;
+            number_count.product = num;
+            // Since this character is a digit,
+            // if the characters to either side are also digits,
+            // they will be a part of the same number,
+            // so we shouldn't check them, and count this number more than once.
+            return number_count;
+        }
+    }
+
+    // Look left.
+    if (pos > 0) {
+        if (wholeNumber(s, pos - 1)) |num| {
+            number_count.count += 1;
+            number_count.product *= num;
+        }
+    }
+
+    // Look right.
+    if (pos < s.len - 1) {
+        if (wholeNumber(s, pos + 1)) |num| {
+            number_count.count += 1;
+            number_count.product *= num;
+        }
+    }
+
+    return number_count;
 }
 
 fn getAnswer(comptime part: comptime_int) ![]const u8 {
