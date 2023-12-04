4597dce7c988eae857f089cefc56306ee79443ba
Niko Carpenter <niko@nikocarpenter.com>
2023-12-03 21:24:36 -0600

    Day 3 part 2

--- zig/2023/day03/src/root.zig
+++ zig/2023/day03/src/root.zig
@@ -47,6 +47,96 @@ fn part1(input: []const u8) !u32 {
     return sum;
 }
 
+fn part2(input: []const u8) !u32 {
+    var lines_iter = std.mem.tokenizeScalar(u8, input, '\n');
+    var prev_line: ?[]const u8 = null;
+    var sum: u32 = 0;
+    var line_num: usize = 0;
+    while (lines_iter.next()) |line| : (prev_line = line) {
+        line_num += 1;
+        const next_line = lines_iter.peek();
+        for (line, 0..) |ch, i| {
+            if (ch != '*') continue;
+            var numbers_count: usize = 0;
+            var ratio: u32 = 1;
+
+            // See if this "geer" is touching exactly two numbers (diagonals included).
+
+            // Look left.
+            if (i > 0) {
+                if (wholeNumber(line, i - 1)) |num| {
+                    numbers_count += 1;
+                    ratio *= num;
+                }
+            }
+            // Look right.
+            if (i < line.len - 1) {
+                if (wholeNumber(line, i + 1)) |num| {
+                    numbers_count += 1;
+                    ratio *= num;
+                }
+            }
+
+            // If there is a digit above or below,
+            // we don't want to check the diagonals on either side,
+            // since they'll either be a part of the same number, or not a digit,
+            // and we don't want to double count the same number.
+
+            // Look above.
+            if (prev_line) |l| {
+                if (wholeNumber(l, i)) |num| {
+                    numbers_count += 1;
+                    ratio *= num;
+                } else {
+                    // Search diagonals on either side.
+                    // Look above and left.
+                    if (i > 0) {
+                        if (wholeNumber(l, i - 1)) |num| {
+                            numbers_count += 1;
+                            ratio *= num;
+                        }
+                    }
+                    // Look above and right.
+                    if (i < l.len - 1) {
+                        if (wholeNumber(l, i + 1)) |num| {
+                            numbers_count += 1;
+                            ratio *= num;
+                        }
+                    }
+                }
+            }
+
+            // Look below.
+            if (next_line) |l| {
+                if (wholeNumber(l, i)) |num| {
+                    numbers_count += 1;
+                    ratio *= num;
+                } else {
+                    // Search diagonals on either side.
+                    // Look below and left.
+                    if (i > 0) {
+                        if (wholeNumber(l, i - 1)) |num| {
+                            numbers_count += 1;
+                            ratio *= num;
+                        }
+                    }
+                    // Look below and right.
+                    if (i < l.len - 1) {
+                        if (wholeNumber(l, i + 1)) |num| {
+                            numbers_count += 1;
+                            ratio *= num;
+                        }
+                    }
+                }
+            }
+
+            if (numbers_count == 2) sum += ratio;
+        }
+    }
+
+    return sum;
+}
+
 /// Returns true if the slice contains a symbol (anything other than '0' - '9', and '.'.
 fn foundSymbol(slice: []const u8) bool {
     if (slice.len == 0) return false;
@@ -57,6 +147,22 @@ fn foundSymbol(slice: []const u8) bool {
         }
     }
     return false;
+}
+
+// Finds the beginning and end of a run of digits,
+// and returns it as an integer.
+// Returns null if pos doesn't refer to a digit.
+fn wholeNumber(s: []const u8, pos: usize) ?u32 {
+    if (pos >= s.len) return null;
+    switch (s[pos]) {
+        '0'...'9' => {},
+        else => return null,
+    }
+
+    var start = pos;
+    while (start > 0 and s[start - 1] >= '0' and s[start - 1] <= '9') : (start -= 1) {}
+    var parser = std.fmt.Parser{ .buf = s, .pos = start };
+    return @intCast(parser.number() orelse unreachable);
 }
 
 fn getAnswer(comptime part: comptime_int) ![]const u8 {
@@ -87,5 +193,30 @@ test "part 1 puzzle input" {
     const input = @embedFile("input.txt");
     const answer = try std.fmt.parseInt(u32, try getAnswer(1), 10);
     const got = try part1(input);
+    try expectEqual(answer, got);
+}
+
+test "part 2 sample input" {
+    const input =
+        \\467..114..
+        \\...*......
+        \\..35..633.
+        \\......#...
+        \\617*......
+        \\.....+.58.
+        \\..592.....
+        \\......755.
+        \\...$.*....
+        \\.664.598..
+    ;
+    const answer = @as(u32, 467835);
+    const got = try part2(input);
+    try expectEqual(answer, got);
+}
+
+test "part 2 puzzle input" {
+    const input = @embedFile("input.txt");
+    const answer = try std.fmt.parseInt(u32, try getAnswer(2), 10);
+    const got = try part2(input);
     try expectEqual(answer, got);
 }
