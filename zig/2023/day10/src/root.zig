c5520b37651678bab0f83901a77458d23dcaf109
Niko Carpenter <niko@nikocarpenter.com>
2023-12-12 12:04:23 -0600

    Day 10 part 2

--- zig/2023/day10/src/root.zig
+++ zig/2023/day10/src/root.zig
@@ -24,7 +24,8 @@ const Direction = enum {
     west,
 };
 
-fn findAnimal(ground: [][]const u8, start_pos: Point, start_direction: Direction, comptime foundVertex: fn ([][]const u8, Point) void) !?usize {
+fn findAnimal(ground: [][]const u8, start_pos: Point, start_direction: Direction, found_vertices: *std.ArrayList(Point)) !?usize {
+    found_vertices.clearRetainingCapacity();
     var pos = start_pos;
     var direction = start_direction;
     const last = Point{ .row = ground.len - 1, .col = ground[0].len - 1 };
@@ -41,12 +42,12 @@ fn findAnimal(ground: [][]const u8, start_pos: Point, start_direction: Direction
             },
             'L' => switch (direction) {
                 .south => blk: {
-                    foundVertex(ground, pos);
+                    try found_vertices.append(pos);
                     direction = .east;
                     break :blk pos.moved(direction, last) orelse return null;
                 },
                 .west => blk: {
-                    foundVertex(ground, pos);
+                    try found_vertices.append(pos);
                     direction = .north;
                     break :blk pos.moved(direction, last) orelse return null;
                 },
@@ -54,12 +55,12 @@ fn findAnimal(ground: [][]const u8, start_pos: Point, start_direction: Direction
             },
             'J' => switch (direction) {
                 .south => blk: {
-                    foundVertex(ground, pos);
+                    try found_vertices.append(pos);
                     direction = .west;
                     break :blk pos.moved(direction, last) orelse return null;
                 },
                 .east => blk: {
-                    foundVertex(ground, pos);
+                    try found_vertices.append(pos);
                     direction = .north;
                     break :blk pos.moved(direction, last) orelse return null;
                 },
@@ -67,12 +68,12 @@ fn findAnimal(ground: [][]const u8, start_pos: Point, start_direction: Direction
             },
             '7' => switch (direction) {
                 .north => blk: {
-                    foundVertex(ground, pos);
+                    try found_vertices.append(pos);
                     direction = .west;
                     break :blk pos.moved(direction, last) orelse return null;
                 },
                 .east => blk: {
-                    foundVertex(ground, pos);
+                    try found_vertices.append(pos);
                     direction = .south;
                     break :blk pos.moved(direction, last) orelse return null;
                 },
@@ -80,42 +81,40 @@ fn findAnimal(ground: [][]const u8, start_pos: Point, start_direction: Direction
             },
             'F' => switch (direction) {
                 .north => blk: {
-                    foundVertex(ground, pos);
+                    try found_vertices.append(pos);
                     direction = .east;
                     break :blk pos.moved(direction, last) orelse return null;
                 },
                 .west => blk: {
-                    foundVertex(ground, pos);
+                    try found_vertices.append(pos);
                     direction = .south;
                     break :blk pos.moved(direction, last) orelse return null;
                 },
                 else => return null,
-            },
-            'S' => blk: {
-                foundVertex(ground, pos); // S may not actually be a vertex, but that's okay
-                break :blk pos;
             },
             '.' => return null,
             else => return error.InvalidTile,
         };
     }
 
+    // `pos` is on the animal (`S` tile).
+    try found_vertices.append(pos);
     return i;
 }
 
-fn findLoop(ground: [][]const u8, point: Point, comptime foundVertex: fn ([][]const u8, Point) void) !usize {
+fn findLoop(ground: [][]const u8, point: Point, found_vertices: *std.ArrayList(Point)) !usize {
     const last = Point{ .row = ground.len - 1, .col = ground[0].len - 1 };
     if (point.moved(.north, last)) |_| {
-        if (try findAnimal(ground, .{ .row = point.row - 1, .col = point.col }, .north, foundVertex)) |steps| return (steps + 1) / 2;
+        if (try findAnimal(ground, .{ .row = point.row - 1, .col = point.col }, .north, found_vertices)) |steps| return steps;
     }
     if (point.moved(.south, last)) |_| {
-        if (try findAnimal(ground, .{ .row = point.row + 1, .col = point.col }, .south, foundVertex)) |steps| return (steps + 1) / 2;
+        if (try findAnimal(ground, .{ .row = point.row + 1, .col = point.col }, .south, found_vertices)) |steps| return steps;
     }
     if (point.moved(.west, last)) |_| {
-        if (try findAnimal(ground, .{ .row = point.row, .col = point.col - 1 }, .west, foundVertex)) |steps| return (steps + 1) / 2;
+        if (try findAnimal(ground, .{ .row = point.row, .col = point.col - 1 }, .west, found_vertices)) |steps| return steps;
     }
     if (point.moved(.east, last)) |_| {
-        if (try findAnimal(ground, .{ .row = point.row, .col = point.col + 1 }, .east, foundVertex)) |steps| return (steps + 1) / 2;
+        if (try findAnimal(ground, .{ .row = point.row, .col = point.col + 1 }, .east, found_vertices)) |steps| return steps;
     }
 
     return error.NoLoopFound;
@@ -136,11 +135,43 @@ fn part1(input: []const u8) !usize {
     }
     if (s_pos == null) return error.NoAnimalFound;
 
-    const foundVertex = (struct {
-        pub fn foundVertex(_: [][]const u8, _: Point) void {}
-    }).foundVertex;
+    var found_vertices = std.ArrayList(Point).init(allocator);
+    defer found_vertices.deinit();
 
-    return findLoop(lines.items, s_pos.?, foundVertex);
+    return (try findLoop(lines.items, s_pos.?, &found_vertices) + 1) / 2;
+}
+
+fn part2(input: []const u8) !usize {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer if (gpa.deinit() == .leak) @panic("Leak!");
+    const allocator = gpa.allocator();
+    var lines = std.ArrayList([]const u8).init(allocator);
+    defer lines.deinit();
+    var s_pos: ?Point = null;
+    var lines_it = std.mem.tokenizeScalar(u8, input, '\n');
+    var i: usize = 0;
+    while (lines_it.next()) |line| : (i += 1) {
+        try lines.append(line);
+        if (std.mem.indexOfScalar(u8, line, 'S')) |j| s_pos = .{ .row = i, .col = j };
+    }
+    if (s_pos == null) return error.NoAnimalFound;
+
+    var found_vertices = std.ArrayList(Point).init(allocator);
+    defer found_vertices.deinit();
+
+    const loop_length = try findLoop(lines.items, s_pos.?, &found_vertices) + 1;
+    // Use the shoelace formula to find the area of the loop
+    var area: isize = 0;
+    i = 0;
+    while (i < found_vertices.items.len) : (i += 1) {
+        const a: isize = @intCast(found_vertices.items[i].row * found_vertices.items[(i + 1) % found_vertices.items.len].col);
+        const b: isize = @intCast(found_vertices.items[i].col * found_vertices.items[(i + 1) % found_vertices.items.len].row);
+        area += (a - b);
+    }
+    if (area < 0) area *= -1;
+    area = @divTrunc(area, 2);
+
+    return @as(usize, @intCast(area + 1)) - @divTrunc(loop_length, 2);
 }
 
 fn getAnswer(comptime part: comptime_int) ![]const u8 {
@@ -182,20 +213,62 @@ test "part 1 puzzle input" {
     try expectEqual(answer, got);
 }
 
-//test "part 2 sample input" {
-//    const input =
-//        \\0 3 6 9 12 15
-//        \\1 3 6 10 15 21
-//        \\10 13 16 21 30 45
-//    ;
-//    const answer: usize = 2;
-//    const got = try part2(input);
-//    try expectEqual(answer, got);
-//}
-//
-//test "part 2 puzzle input" {
-//    const input = @embedFile("input.txt");
-//    const answer = try std.fmt.parseInt(usize, try getAnswer(2), 10);
-//    const got = try part2(input);
-//    try expectEqual(answer, got);
-//}
+test "part 2 sample 1 input" {
+    const input =
+        \\...........
+        \\.S-------7.
+        \\.|F-----7|.
+        \\.||.....||.
+        \\.||.....||.
+        \\.|L-7.F-J|.
+        \\.|..|.|..|.
+        \\.L--J.L--J.
+        \\...........
+    ;
+    const answer: usize = 4;
+    const got = try part2(input);
+    try expectEqual(answer, got);
+}
+
+test "part 2 sample 2 input" {
+    const input =
+        \\.F----7F7F7F7F-7....
+        \\.|F--7||||||||FJ....
+        \\.||.FJ||||||||L7....
+        \\FJL7L7LJLJ||LJ.L-7..
+        \\L--J.L7...LJS7F-7L7.
+        \\....F-J..F7FJ|L7L7L7
+        \\....L7.F7||L7|.L7L7|
+        \\.....|FJLJ|FJ|F7|.LJ
+        \\....FJL-7.||.||||...
+        \\....L---J.LJ.LJLJ...
+    ;
+    const answer: usize = 8;
+    const got = try part2(input);
+    try expectEqual(answer, got);
+}
+
+test "part 2 sample 3 input" {
+    const input =
+        \\FF7FSF7F7F7F7F7F---7
+        \\L|LJ||||||||||||F--J
+        \\FL-7LJLJ||||||LJL-77
+        \\F--JF--7||LJLJ7F7FJ-
+        \\L---JF-JLJ.||-FJLJJ7
+        \\|F|F-JF---7F7-L7L|7|
+        \\|FFJF7L7F-JF7|JL---7
+        \\7-L-JL7||F7|L7F-7F7|
+        \\L.L7LFJ|||||FJL7||LJ
+        \\L7JLJL-JLJLJL--JLJ.L
+    ;
+    const answer: usize = 10;
+    const got = try part2(input);
+    try expectEqual(answer, got);
+}
+
+test "part 2 puzzle input" {
+    const input = @embedFile("input.txt");
+    const answer = try std.fmt.parseInt(usize, try getAnswer(2), 10);
+    const got = try part2(input);
+    try expectEqual(answer, got);
+}
