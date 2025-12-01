823b4b2e42a635b82fcb7ead66c1fa48b799c263
Niko Carpenter <niko@nikocarpenter.com>
2025-12-01 10:36:37 -0600

    Clean up code, add comments, and known correct answers (only for my puzzle input)

--- bash/2025/day01/part1.sh
+++ bash/2025/day01/part1.sh
@@ -3,12 +3,12 @@
 lock=50
 password=0
 while read -r line; do
-    rotations="${line:1}"
-    case "$line" in
-    L*) ((lock -= rotations)) ;;
-    R*) ((lock += rotations)) ;;
-    esac
-    lock=$(((100 + lock % 100) % 100))
+    rotations="${line:1}" # Strips the L/R
+    [[ $line = L* ]] && ((rotations*=-1))
+    # Lock must always be an integer between 0 and 99,
+    # but in Bash, x % y, where x < 0 is negative.
+    # The 100 + ... % 100 handles that.
+    lock=$(((100 + lock+rotations % 100) % 100))
     [[ $lock -eq 0 ]] && ((password++))
 done
 
--- bash/2025/day01/part2.sh
+++ bash/2025/day01/part2.sh
@@ -3,19 +3,19 @@
 lock=50
 password=0
 while read -r line; do
-    rotations="${line:1}"
-    case "$line" in
-    L*)
-        ((lock -= rotations))
-        [[ $lock -ne -$rotations ]] && ((lock -= 100))
-        ;;
-    R*) ((lock += rotations)) ;;
-    esac
-    zeros=$((lock / 100))
+    rotations="${line:1}" # Strips the L/R
+    [[ $line = L* ]] && ((rotations*=-1))
+    # If lock > 0, and we're rotating left, the initial zero crossing isn't handled by integer division.
+    # For example lock= 50, L68 -> lock = -18 (82 after modulo).
+    # -18/100 = 0, but there was 1 crossing over zero.
+    [[ $lock -gt 0 && $rotations -lt 0 ]] && ((rotations-=100))
+    zeros=$(((lock+rotations) / 100))
     zeros="${zeros#-}"
     ((password += zeros))
-    lock=$(((100 + lock % 100) % 100))
-    #echo "$line $lock $password"
+    # Lock must always be an integer between 0 and 99,
+    # but in Bash, x % y, where x < 0 is negative.
+    # The 100 + ... % 100 handles that.
+    lock=$(((100 + lock+rotations % 100) % 100))
 done
 
 echo "$password"
