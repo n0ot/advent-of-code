7bec8d0eff337c6b4eb188c96f74f170aa95a642
Niko Carpenter <niko@nikocarpenter.com>
2025-12-03 17:24:35 -0600

    simplify max jolt vars

--- bash/2025/day03/max-jolts.sh
+++ bash/2025/day03/max-jolts.sh
@@ -11,21 +11,19 @@ num_jolt_digits="$1"
 sum=0
 while read -r line; do
     digits=
-    start=0
+    max_idx=-1
     for ((i = 0; i < num_jolt_digits; i++)); do
         end=$((${#line} - num_jolt_digits+i))
-        max_idx=
         max_digit=0
-        for ((j = start; j <= end; j++)); do
+        for ((j = max_idx+1; j <= end; j++)); do
             digit="${line:j:1}"
-            if [[ -z $max_idx || $digit -gt $max_digit ]]; then
+            if [[ $digit -gt $max_digit ]]; then
                 max_idx=$j
                 max_digit="$digit"
             fi
             [[ $digit -eq 9 ]] && break
         done
         digits="${digits}$max_digit"
-        start=$((max_idx+1))
     done
     ((sum+=digits))
 done
