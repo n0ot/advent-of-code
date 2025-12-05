83e9f81d7f62590b54d0b6263f35bf4598294e04
Niko Carpenter <niko@nikocarpenter.com>
2025-12-05 12:25:12 -0600

    update overlap logic

--- bash/2025/day05/helper.sh
+++ bash/2025/day05/helper.sh
@@ -2,24 +2,29 @@
 
 read_ranges() {
     local -n _ranges_ref="$1"
-    local range this_start this_end existing_range existing_start existing_end add_this_range
+    local range this_start this_end existing_range existing_start existing_end
     while read -r range; do
         [[ -z $range ]] && break
         # Expand existing ranges if this one overlaps
         IFS=- read -r this_start this_end <<<"$range"
-        add_this_range=yes
         for existing_range in "${!_ranges_ref[@]}"; do
             IFS=- read -r existing_start existing_end <<<"$existing_range"
+            if [[ $existing_start -le $this_start && $this_end -le $existing_end ]]; then
+                continue 2 # This range is fully encompassed by another
+            fi
             if [[ $this_start -le $existing_start && $existing_start -le $this_end ]]; then
+                # the existing range overlaps this one on the right
+                # Expand this range to the right, unless existing range is fully encompassed.
                 [[ $existing_end -gt $this_end ]] && this_end="$existing_end"
                 unset '_ranges_ref['"$existing_range"']'
             elif [[ $this_start -le $existing_end && $existing_end -le $this_end ]]; then
-                [[ $existing_start -lt $this_start ]] && this_start="$existing_start"
+                # the existing range overlaps this one on the left
+                # Expand this range to the left.
+                # The case of this range being fully encompassed is already handled.
+                this_start="$existing_start"
                 unset '_ranges_ref['"$existing_range"']'
-            elif [[ $existing_start -le $this_start && $this_end -le $existing_end ]]; then
-                add_this_range=no
             fi
         done
-        [[ $add_this_range = yes ]] && _ranges_ref["$this_start-$this_end"]=
+        _ranges_ref["$this_start-$this_end"]=
     done
 }
